3a93e3aa846062354089bb2eef64e019dc4cd586
A. Cody Schuffelen <schuffelen@google.com>
2025-12-08 19:21:18 -0800

    PartitionSource

---
+++ base/cvd/cuttlefish/host/commands/assemble_cvd/android_build/partition_source.h
@@ -0,0 +1,47 @@
+//
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <ostream>
+#include <string_view>
+
+#include "fmt/ostream.h"
+
+namespace cuttlefish {
+
+enum class PartitionSource {
+  kUnknown,
+  kVendor,
+  kSystem,
+};
+
+std::string_view ToStringView(PartitionSource);
+
+std::ostream& operator<<(std::ostream&, PartitionSource);
+
+template <typename Sink>
+void AbslStringify(Sink& sink, PartitionSource ab_status) {
+  sink.Append(ToStringView(ab_status));
+}
+
+}  // namespace cuttlefish
+
+namespace fmt {
+
+template <>
+struct formatter<::cuttlefish::PartitionSource> : ostream_formatter {};
+
+}  // namespace fmt
